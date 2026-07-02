f9dc73758fc5983fc213ce8d13ed92739f172efc
jazz212 <livelyn12.lc@gmail.com>
2026-07-02 14:32:04 +0800

    Arrange the files and folders and creation of Login.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,18 @@
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import Dashboard from "../components/Dashboard";
+
+export default function Home() {
+  return (
+    /**/
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Navbar />
+        <div className="overflow-y-auto flex-1">
+          <Dashboard />
+        </div>
+      </div>
+    </div>
+  );
+}
